6271d7b4d49e630ec1c7f0f91296e4032142f797
Peng Minggao <1242768252@qq.com>
2025-10-31 14:08:52 +0800

    implPass2

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -1,11 +1,13 @@
-# 2025-10-22T21:58:13.540035
+# 2025-10-31T13:46:36.676307700
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="PROJECT")
 
-comp = client.get_component(name="cholesky_test")
-comp.run(operation="CO_SIMULATION")
+comp = client.get_component(name="lz4_cmpress")
+comp.run(operation="IMPLEMENTATION")
+
+comp = client.create_hls_component(name = "cholesky_test",cfg_file = ["hls_config.cfg"],template = "empty_hls_component")
 
 comp.run(operation="C_SIMULATION")
 
@@ -13,7 +15,5 @@ comp.run(operation="SYNTHESIS")
 
 comp.run(operation="CO_SIMULATION")
 
-client.delete_component(name="cholesky_test")
-
-vitis.dispose()
+comp.run(operation="IMPLEMENTATION")
 
